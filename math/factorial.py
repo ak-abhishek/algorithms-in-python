a86466009769fb4e75821188562afcb68ade49d7
Shreyas <shrykulk.rk85@gmail.com>
2015-08-07 23:41:24 +0530

    Updating factorial and fibonacci

---
+++ math/factorial.py
@@ -0,0 +1,8 @@
+def factorial(number):
+	if number == 0:
+		return 1
+	else:
+		return number * factorial(number - 1)
+
+def factorial2(number):
+	return reduce(lambda x, y: x * y, [i for i in range(1, number + 1), 1])
---
+++ math/fibonacci.py
@@ -0,0 +1,17 @@
+class Fibonacci:
+	def __init__(self, number):
+		self.d = {0: 1, 1: 1, 2: 2}
+		self.n = number
+		if self.n > 2:
+			for i in range(3, self.n):
+				self.d[i] = self.d[i-1] + self.d[i-2]
+	
+	def fibonacci_nth_number(self):
+		return self.d[self.n - 1]
+
+	def sum_of_fibonacci_numbers(self):
+		return sum(self.d.values())
+
+if __name__ == '__main__':
+	print Fibonacci(100).fibonacci_nth_number()
+	print Fibonacci(100).sum_of_fibonacci_numbers()
