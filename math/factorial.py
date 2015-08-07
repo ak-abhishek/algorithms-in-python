266764191ac6a102399cd024faddb2bfb6264c1c
Shreyas <shrykulk.rk85@gmail.com>
2015-08-07 23:51:40 +0530

    Removed indentation issues

--- math/factorial.py
+++ math/factorial.py
@@ -1,8 +1,9 @@
 def factorial(number):
-	if number == 0:
-		return 1
-	else:
-		return number * factorial(number - 1)
+    if number == 0:
+        return 1
+    else:
+        return number * factorial(number - 1)
+
 
 def factorial2(number):
-	return reduce(lambda x, y: x * y, [i for i in range(1, number + 1), 1])
+    return reduce(lambda x, y: x * y, [i for i in range(1, number + 1), 1])
